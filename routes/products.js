0cc21ea0fd516277aaf4ee58f3df5c81ee4730c7
Fardinsk7 <itzfardinsk@gmail.com>
2024-01-22 18:27:44 +0530

    Payment Integration test 3

--- routes/products.js
+++ routes/products.js
@@ -85,29 +85,35 @@ router.delete("/delete/:id",async(req,res)=>{
 router.post("/createCheckoutSession", async(req,res)=>{
     const{products} = req.body;
     console.log("Hello Stripe");
-    const lineItems = products.map((product)=>({
-        price_data:{
-            currency:"inr",
-            product_data:{
-                name: product.name,
-                images:[product.image]
+    try {
+        const lineItems = products.map((product)=>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name: product.name,
+                    images:[product.image]
+                },
+                unit_amount: Math.round(product.price*100)
             },
-            unit_amount: Math.round(product.price*100)
-        },
-        quantity: product.amount
-    }));
-
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types:["card"],
-        line_items: lineItems,
-        mode:"payment",
-        success_url:"https://caymanlaptops.netlify.app/paymentSuccess",
-        cancel_url:"https://caymanlaptops.netlify.app/paymentFail",
-        shipping_address_collection: {
-            allowed_countries: ['US', 'CA', 'GB', 'AU', 'IN'], // Add the two-letter country code for India
-        },
-    })
-    res.json({id:session.id})
+            quantity: product.amount
+        }));
+    
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+            line_items: lineItems,
+            mode:"payment",
+            success_url:"https://caymanlaptops.netlify.app/paymentSuccess",
+            cancel_url:"https://caymanlaptops.netlify.app/paymentFail",
+            shipping_address_collection: {
+                allowed_countries: ['US', 'CA', 'GB', 'AU', 'IN'], // Add the two-letter country code for India
+            },
+        })
+        res.json({id:session.id})
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:error})
+    }
     
 })
 
